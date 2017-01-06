22d4a92bca710af52f55ffb7caff63f9f0ceb381
slattery <michael.slattery@yale.edu>
2017-01-06 16:43:44 -0500

    WIP ruby var basics

--- app/jobs/scheduled/fixed_daily_digest.rb
+++ app/jobs/scheduled/fixed_daily_digest.rb
@@ -3,19 +3,18 @@ module Jobs
   class EnqueueFixedDigestEmails < Jobs::Scheduled
     every 15.minutes
 
-    def execute(args)
-      #match_day = Time.parse(Time.now.in_time_zone('America/New_York'))
+
+      
+    def execute(args)    
       match_hrs = Time.now.in_time_zone('America/New_York').hour
-      #match_day.hour.hour
       match_arr = %w[0000 0100 0200 0300 0400 0500 0600 0700 0800 0900 1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000 2100 2200 2300];
-      match_str = match_arr.at(match_hrs) || '0000'    
-    
-    
+      @match_str = match_arr.at(match_hrs) || '0000'
+
       if SiteSetting.fixed_digest_enabled?
         if match_hrs >= 7 && match_hrs <= 16
-          Rails.logger.warn("fixed summaries trying to match users for #{match_str}")
+          Rails.logger.warn("fixed summaries trying to match users for #{@match_str}")
           target_user_ids.each do |user_id|
-            Rails.logger.warn("fixed summaries trying to send digest to #{user_id} for #{match_str} delivery")
+            Rails.logger.warn("fixed summaries trying to send digest to #{user_id} for #{@match_str} delivery")
             Jobs.enqueue(:user_email, type: :mailing_list, user_id: user_id)
           end
         end
@@ -29,7 +28,7 @@ module Jobs
                   .joins(:user_custom_fields)
                   .not_suspended
                   .where(user_custom_fields: {fixed_digest_emails: true})
-                  .where("user_custom_fields.fixed_digest_deliveries REGEXP ?", "#{match_str}")
+                  .where("user_custom_fields.fixed_digest_deliveries REGEXP ?", "#{@match_str}")
                   
       # If the site requires approval, make sure the user is approved
       if SiteSetting.must_approve_users?
