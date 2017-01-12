56c8f6c325c9d130ae36be0e50df5cf86d65cd3e
slattery <michael.slattery@yale.edu>
2017-01-12 13:04:17 -0500

    WIP no timze zone

--- app/jobs/scheduled/fixed_daily_digest.rb
+++ app/jobs/scheduled/fixed_daily_digest.rb
@@ -1,7 +1,7 @@
 module Jobs
   # A daily job that will enqueue digest emails to be sent to users at fixed times
   class EnqueueFixedDigestEmails < Jobs::Scheduled
-    at 13.hours.in_time_zone('America/New_York')
+    at 6.hours
       
     def execute(args)    
       if SiteSetting.fixed_digest_enabled?
