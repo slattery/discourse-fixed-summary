0ad2502cd4f206fafa3b74fa9bb9fa6dc4cd742f
slattery <michael.slattery@yale.edu>
2019-08-23 16:46:35 -0400

    better working version of user prefs

--- plugin.rb
+++ plugin.rb
@@ -7,41 +7,15 @@
 enabled_site_setting :fixed_digest_enabled
 
 after_initialize do
-  require_dependency 'user_serializer'
 
-  public_user_custom_fields_setting = SiteSetting.public_user_custom_fields
 
-  if public_user_custom_fields_setting.empty?
-    SiteSetting.set("public_user_custom_fields", "fixed_digest_emails|fixed_digest_deliveries")
-  else
-    if public_user_custom_fields_setting !~ /fixed_digest_emails/
-      SiteSetting.set(
-        "public_user_custom_fields",
-        [SiteSetting.public_user_custom_fields, "fixed_digest_emails"].join("|")
-      )
-    end
-    if public_user_custom_fields_setting !~ /fixed_digest_deliveries/
-      SiteSetting.set(
-        "public_user_custom_fields",
-        [SiteSetting.public_user_custom_fields, "fixed_digest_deliveries"].join("|")
-      )
-    end
-  end
+  register_editable_user_custom_field :fixed_digest_emails
+  register_editable_user_custom_field :fixed_digest_deliveries
 
-  class ::UserSerializer
-    alias_method :_custom_fields, :custom_fields
-    def custom_fields
-      if !object.custom_fields["fixed_digest_emails"]
-        object.custom_fields["fixed_digest_emails"] = ""
-        object.save
-      end
-      if !object.custom_fields["fixed_digest_deliveries"]
-        object.custom_fields["fixed_digest_deliveries"] = "0900|1600"
-        object.save
-      end
-      _custom_fields
-    end
-  end
+  User.register_custom_field_type 'fixed_digest_emails', :boolean
+
+  DiscoursePluginRegistry.serialized_current_user_fields << 'fixed_digest_emails'
+  DiscoursePluginRegistry.serialized_current_user_fields << 'fixed_digest_deliveries'
 
   [
     '../app/jobs/scheduled/fixed_daily_digest.rb',
